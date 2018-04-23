8f522547d6aeca3cdab77209b5db3df9d0eb3b30
Nathan-Wen <36411251+Nathan-Win-Wen@users.noreply.github.com>
2018-04-23 07:23:46 -0400

    Video #12

---
+++ app/models/Book.java
@@ -0,0 +1,78 @@
+package models;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Book {
+
+    private Integer id;
+    private String title;
+    private Integer price;
+    private String author;
+
+    public Book(Integer id, String title, Integer price, String author) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.author = author;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public Integer getId() {
+        return id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public String getAuthor() {
+        return author;
+    }
+
+
+    private static Set<Book> books;
+
+    static {
+        books = new HashSet<>();
+        books.add(new Book(1,"C++",20,"ABC"));
+        books.add(new Book(2,"Java",30,"ABC"));
+    }
+
+    public static Set<Book> allbooks(){
+        return books;
+    }
+
+    public static Book findById(Integer id) {
+        for (Book book : books) {
+            if(id.equals(book.id)) {
+                return book;
+            }
+        }
+
+        return null;
+    }
+
+    public static void add(Book book) {
+        books.add(book);
+    }
+
+    public static boolean remove(Book book) {
+        return books.remove(book);
+    }
+}
